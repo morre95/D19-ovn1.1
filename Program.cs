4501296c9a98f6207e7795a8e5e473cca7cc1805
morre95 <erik.moren78@gmail.com>
2023-03-02 15:48:48 +0100

    Har ändrat massa kod

--- Program.cs
+++ Program.cs
@@ -52,35 +52,6 @@ namespace D19_ovn1._1
             Console.WriteLine($"Sorry: '{command}' is not yet implemented");
         }
 
-
-
-<<<<<<< HEAD
-                
-
-            }
-            return ret;
-
-        }
-
-        private static string GetLink(string search)
-=======
-        private static void GetLink()
->>>>>>> 173e32ee88cd6881e3b4873944a51dc5863a3317
-        {
-            string ret = null;
-            string[] lines = File.ReadAllLines(@"C:\Users\Erik\source\repos\D19-ovn1.1\linkLinst.list");   
-            foreach (string line in lines)
-            {
-                string[] test = line.Split("|");
-
-
-                Console.WriteLine(test[0]);
-                Console.WriteLine(test[1]);
-                Console.WriteLine(test[2]);
-
-            }
-        }
-
         private static void PrintWelcome()
         {
             Console.WriteLine("Hello and welcome to the ... program ...");
--- load.cs
+++ load.cs
@@ -6,13 +6,18 @@ namespace D19_ovn1._1
 
     public static class load
     {
-        public static void All(string URL)
+        public static void All()
         {
-            Process process = new Process();
-            process.StartInfo.UseShellExecute = true;
-            process.StartInfo.FileName = URL;
-            process.Start();
-            process.WaitForExit();
+            string path = Directory.GetCurrentDirectory();
+            string[] lines = File.ReadAllLines($@"{path}\linkList.list");
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split("|");
+                Process process = new Process();
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.FileName = parts[2];
+                process.Start();
+            }
         }
     }
 }
