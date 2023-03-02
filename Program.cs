5a8b999b7fd61718f43644548855fc583c12d8d8
morre95 <erik.moren78@gmail.com>
2023-03-02 11:19:31 +0100

    Har klistrat in kåden

--- Program.cs
+++ Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintWelcome();
+            string command;
+            do
+            {
+                Console.Write(": ");
+                command = Console.ReadLine();
+                if (command == "quit")
+                {
+                    Console.WriteLine("Good bye!");
+                }
+                else if (command == "help")
+                {
+                    WriteTheHelp();
+                }
+                else if (command == "load")
+                {
+                    NotYetImplemented(command);
+                }
+                else if (command == "open")
+                {
+                    NotYetImplemented(command);
+                }
+                else
+                {
+                    Console.WriteLine($"Unknown command '{command}'");
+                }
+            } while (command != "quit");
+        }
+
+        private static void NotYetImplemented(string command)
+        {
+            Console.WriteLine($"Sorry: '{command}' is not yet implemented");
+        }
+
+        private static void PrintWelcome()
+        {
+            Console.WriteLine("Hello and welcome to the ... program ...");
+            Console.WriteLine("that does ... something.");
+            Console.WriteLine("Write 'help' for help!");
+        }
+
+        private static void WriteTheHelp()
+        {
+            string[] hstr = {
+                "help  - display this help",
+                "load  - load all links from a file",
+                "open  - open a specific link",
+                "quit  - quit the program"
+            };
+            foreach (string h in hstr) Console.WriteLine(h);
         }
     }
 }
