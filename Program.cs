11add15226591f7b710aaa5f39b9fa5386751d7b
morre95 <erik.moren78@gmail.com>
2023-03-02 11:10:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D19_ovn1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
