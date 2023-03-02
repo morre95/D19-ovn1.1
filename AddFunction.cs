3462d63ca3178ec7e3ab838de2cefac38c37a543
Madi015 <madisaed2003@yahoo.com>
2023-03-02 15:31:34 +0100

    första försök

--- AddFunction.cs
+++ AddFunction.cs
@@ -6,7 +6,37 @@ namespace D19_ovn1._1
 	{
 		public static void addFunction()
 		{
-			Console.WriteLine(" här är add funktion");
-		}
-	}
+            Console.WriteLine("vill du lägg till nya länkar? skriv J for ja elle N för nej");
+            string svaret = Console.ReadLine();
+            if (svaret == "J")
+            {
+                int grans = 0;
+                Console.WriteLine("Hur många länkar vill du mata in=");
+                grans = Int32.Parse((Console.ReadLine()));
+                for (int i = 0; i < grans; i++)
+                {
+                    Console.WriteLine("Skriv namnet: ");
+                    string namn = Console.ReadLine();
+                    Console.WriteLine("Skriv kort beskrivning: ");
+                    string beskrivning = Console.ReadLine();
+                    Console.WriteLine("Skriv lanken, t.e. www.Google.com ");
+                    string lanken = Console.ReadLine();
+                    string[] nylank = { namn, beskrivning, lanken };
+                    string inmatning = nylank[0] + "|" + nylank[1] + "|" + nylank[2];
+                    File.WriteAllText(@"C:\Users\Erik\source\repos\D19-ovn1.1\linkLinst.list", inmatning);
+                }
+                Console.WriteLine("Vill du lägga till mer länkar?");
+                
+            }
+            else
+            {
+                Console.WriteLine("Tack för att du använder vår tjänst.");
+            }
+            
+            
+
+            
+
+        }
+    }
 }
